2b92bdbde6aadbbeef42d9b1fb19d867d2513a1a
Drincann <1019933576@qq.com>
2021-12-20 15:36:11 +0800

    feature(back): 实现了获取已上传的文件信息接口

--- index.ts
+++ index.ts
@@ -9,6 +9,34 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/info', async (req, res) => {
+    const time2files: { [time: string]: { url: string, name: string }[] } = {}
+    const resData: {
+        items?: {
+            time: string,
+            files: { url: string, name: string }[]
+        }[]
+    } = { items: [] }
+
+    const dirents = await fs.readdir(uploads, { withFileTypes: true })
+
+    for (const item of dirents) {
+        const stat = await fs.stat(path.join(uploads, item.name))
+        // 时间取当天凌晨
+        const date = new Date(stat.birthtime)
+        date.setHours(0, 0, 0, 0)
+        // 转换为 年/月/日
+        const dateStr = date.toISOString().slice(0, 10)
+        if (!time2files[dateStr]) {
+            time2files[dateStr] = []
+        }
+        time2files[dateStr].push({ url: `/uploads/${item.name}`, name: item.name })
+    }
+    for (const time in time2files) {
+        resData.items?.push({ time, files: time2files[time] })
+    }
+    res.send(responseData(RESCODE.SUCCESS, resData))
+})
 app.post('/upload', multer({ limits: { fileSize: 1024 * 1024 * 50 } }).single('codePack',), async (req, res) => {
     try {
         if (req.body?.groupId?.trim?.() === '') {
@@ -34,7 +62,7 @@ app.post('/upload', multer({ limits: { fileSize: 1024 * 1024 * 50 } }).single('c
             ))
         }
 
-        await fs.writeFile(path.join(uploads, `第${groupId}组${fileExt}`), buffer)
+        await fs.writeFile(path.join(uploads, `${groupId}${fileExt}`), buffer)
         res.send(responseData(RESCODE.SUCCESS, { message: 'upload success' }))
     } catch (error: any) {
         res.json(responseData(RESCODE.FAIL, { message: error?.message ?? 'unknown error' }))
